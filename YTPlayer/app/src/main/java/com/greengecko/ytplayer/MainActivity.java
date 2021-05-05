6a0c96995c9b2d9f677205369acbdf7ea1058fcc
HyeongminKim <assa0620@gmail.com>
2021-05-06 01:39:21 +0900

    메인 화면 리스너 구축

--- YTPlayer/app/src/main/java/com/greengecko/ytplayer/MainActivity.java
+++ YTPlayer/app/src/main/java/com/greengecko/ytplayer/MainActivity.java
@@ -2,13 +2,60 @@ package com.greengecko.ytplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button explore, library, setting, info;
+    Intent transition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void action(Activity target) {
+        transition = new Intent(getApplicationContext(), Activity.class);
+        startActivity(transition);
+    }
+
+    private void init() {
+        explore = findViewById(R.id.explore);
+        library = findViewById(R.id.library);
+        setting = findViewById(R.id.setting);
+        info = findViewById(R.id.info);
     }
 }
