97741ac1f23cb8acf155e6b8651ef55cd7858787
HyeongminKim <assa0620@gmail.com>
2021-05-06 14:50:32 +0900

    코드 최적화 및 탐색, 라이브러리 뷰 뷰어 설정

--- YTPlayer/app/src/main/java/com/greengecko/ytplayer/MainActivity.java
+++ YTPlayer/app/src/main/java/com/greengecko/ytplayer/MainActivity.java
@@ -2,32 +2,35 @@ package com.greengecko.ytplayer;
 
 import android.app.TabActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
 
+import java.util.List;
+
 public class MainActivity extends TabActivity {
+    TabHost host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost host = getTabHost();
+        init();
+        setAction();
+    }
+
+    private void init() {
+        host = getTabHost();
 
         tabAdder(host, "HOME", "홈", R.id.tabHome);
         tabAdder(host, "EXPLORE", "탐색", R.id.tabExplore);
         tabAdder(host, "LIBRARY", "라이브러리", R.id.tabLibrary);
         tabAdder(host, "SETTING", "설정", R.id.tabSetting);
-
-        host.setCurrentTab(0);
-    }
-
-    private void init() {
-
     }
 
     private void setAction() {
-
+        host.setCurrentTab(0);
     }
 
     private void tabAdder(TabHost host, String tag, String indicator, int viewId) {
