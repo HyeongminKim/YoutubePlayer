b94544e4ffca2faf954afb7c90c4b18c4501032e
HyeongminKim <assa0620@gmail.com>
2021-05-12 13:28:21 +0900

    플레이어 마이너 버그 픽스

--- YTPlayer/app/src/main/java/com/greengecko/ytplayer/MediaPlayer.java
+++ YTPlayer/app/src/main/java/com/greengecko/ytplayer/MediaPlayer.java
@@ -10,6 +10,7 @@ import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -27,34 +28,46 @@ public class MediaPlayer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
         init();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
-        playerView.setPlayer(player);
-        DataSource.Factory factory = new DefaultDataSourceFactory(this, "YTExoPlayer");
-        ProgressiveMediaSource source = new ProgressiveMediaSource.Factory(factory).createMediaSource(videoSrc);
-        player.prepare(source);
-
-        player.setPlayWhenReady(true);
+        startService();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        player.setPlayWhenReady(false);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         playerView.setPlayer(null);
         player.release();
         player = null;
     }
 
     private void init() {
+        setContentView(R.layout.activity_media_player);
+
         playerView = findViewById(R.id.player);
 
         intent = getIntent();
         videoSrc = Uri.parse(intent.getExtras().getString("src"));
+    }
+
+    private void startService() {
+        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
+        playerView.setPlayer(player);
+        playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH);
+        DataSource.Factory factory = new DefaultDataSourceFactory(this, "YTExoPlayer");
+        ProgressiveMediaSource source = new ProgressiveMediaSource.Factory(factory).createMediaSource(videoSrc);
+        player.prepare(source);
+
+        player.setPlayWhenReady(true);
     }
 }
